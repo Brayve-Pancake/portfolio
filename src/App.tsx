858ca451dd40da2fe631f3c47395d565db9a4065
Joshua Bray <josh.c.bray@gmail.com>
2022-08-31 13:13:55 +0100

    Add initial color scheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "../sass-boilerplate/stylesheets/main.scss";
-import { Footer } from "./components/index";
+import { Footer, Navbar } from "./components/index";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       hello
       <Footer />
     </div>
