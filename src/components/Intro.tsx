3e27da6bb6c0fb5076b3a6b74644fb10e5c90a5d
Joshua Bray <josh.c.bray@gmail.com>
2022-09-01 14:59:56 +0100

    Remove whitespace

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,7 +1,10 @@
 export default function Intro() {
   return (
     <div className="intro">
-      <p className="intro-text">Hi, I'm Joshua...</p>
+      <p className="intro-text">
+        Hi, I'm Joshua... Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Esse dolores eos aliquam error quam perferendis!
+      </p>
     </div>
   );
 }
